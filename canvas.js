60424424a0bb4b8301b42b409e6a71b6a2941b0b
aprtlt26 <117754660+aprtlt26@users.noreply.github.com>
2023-02-21 13:48:06 -0500

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -1,15 +1,14 @@
 canvas.addEventListener('touchstart', function(event) {
-    event.preventDefault();
-    startPainting();
-  });
-  
-  canvas.addEventListener('touchend', function(event) {
-    event.preventDefault();
-    stopPainting();
-  });
-  
-  canvas.addEventListener('touchmove', function(event) {
-    event.preventDefault();
-    paint(event.touches[0]);
-  });
-  
+  event.preventDefault();
+  startPainting();
+}, {passive: true});
+
+canvas.addEventListener('touchend', function(event) {
+  event.preventDefault();
+  stopPainting();
+});
+
+canvas.addEventListener('touchmove', function(event) {
+  event.preventDefault();
+  paint(event.touches[0]);
+}, {passive: true});
